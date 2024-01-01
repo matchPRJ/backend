c5daff363a34219cfd2ba60dbb116397675cc4c1
taeyoung kang <kty1467@naver.com>
2024-01-01 15:18:52 +0900

    [변경] 검색 코드 오류 수정 테스트 완료

--- src/main/java/com/team/match/controller/FileUploadController.java
+++ src/main/java/com/team/match/controller/FileUploadController.java
@@ -1,11 +1,15 @@
 package com.team.match.controller;
 
+import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
@@ -13,9 +17,10 @@ import java.io.IOException;
 @RestController
 @RequestMapping("/upload")
 @RequiredArgsConstructor
+@Component
 public class FileUploadController {
 
-    private final AmazonS3Client S3Client;
+    private final AmazonS3 S3Client;
 
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
